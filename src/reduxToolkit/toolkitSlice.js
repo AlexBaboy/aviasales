e940c6f005bfd56992aee9aaf11358c2fb159474
baboy <baboy@prisbank.com>
2020-12-11 17:11:23 +0200

    extra reducers

--- src/reduxToolkit/toolkitSlice.js
+++ src/reduxToolkit/toolkitSlice.js
@@ -1,5 +1,4 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import getTickets from "../components/ticketList/TicketsList";
 import axios from "axios";
 
 const setTicketsInitial = createAsyncThunk(
